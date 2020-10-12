6ebcf104b14766768f7d8b89a3c55de4fa8d0c20
Firus Hanov <d00363593@dmail.dixie.edu>
2020-10-11 22:43:38 -0600

    frontend finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+// as is used as ALIAS for BrowserRouter library
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-    <>
-      <h1>Welcome to ProShow</h1>
-    </>
-      
-    </div>
+    <Router>
+    <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen}></Route>
+        </Container>
+      </main>
+    <Footer />
+    </Router>
   );
 }
 
